2686defc7f6c345bfb18b266ca485e6fe7a102e3
fadelmuhammad97 <36094884+fadelmuhammad97@users.noreply.github.com>
2018-04-03 09:12:20 +0700

    create a new for detail,menu,adapter

---
+++ app/src/main/java/com/example/padhil/mofo/DetailGunung.java
@@ -0,0 +1,20 @@
+package com.example.padhil.mofo;
+
+import android.media.Image;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
+
+import java.util.List;
+
+
+/**
+ * Created by fadel muhammad rizki on 3/13/2018.
+ */
+
+public class DetailGunung extends AppCompatActivity {
+    RecyclerView recv;
+    Adaptermenu adptr;
+    List<menulist> DetailGunung;
+
+}
