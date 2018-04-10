5fccec3ea087f6f3d2fd2d0a1395a19ab40cfbfa
fadelmuhammad97 <36094884+fadelmuhammad97@users.noreply.github.com>
2018-04-10 07:53:30 +0700

    ini nih

--- app/src/main/java/com/example/padhil/mofo/DetailGunung.java
+++ app/src/main/java/com/example/padhil/mofo/DetailGunung.java
@@ -3,6 +3,7 @@ package com.example.padhil.mofo;
 import android.media.Image;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.RecyclerView;
 import android.widget.ImageView;
 
 import java.util.List;
