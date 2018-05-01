057ca05cc08f1bba32f118d976f934e538d15fff
Azkya Telisha <azkyatelisha@gmail.com>
2018-05-01 21:42:33 +0700

    hasil azkya selama ini

--- app/src/main/java/com/example/padhil/mofo/MainMenu.java
+++ app/src/main/java/com/example/padhil/mofo/MainMenu.java
@@ -5,17 +5,28 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
+import com.google.firebase.auth.FirebaseAuth;
+
 
 public class MainMenu extends AppCompatActivity {
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
+        mAuth = FirebaseAuth.getInstance();
     }
 
-    public void forcast(View view) {
-        Intent l1 = new Intent(MainMenu.this, MenuForcastActivity.class);
-        startActivity(l1);
+    public void goWeather(View view) {
+        Intent weather = new Intent(MainMenu.this, MenuForcastActivity.class);
+        startActivity(weather);
+    }
+
+    public void logout(View view){
+            mAuth.signOut();
+            Intent intent = new Intent(MainMenu.this, LoginActivity.class);
+            startActivity(intent);
+            finish();
     }
 }
